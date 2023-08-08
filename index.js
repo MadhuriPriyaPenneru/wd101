94a05392a3a47fd5bfff2ce264c795da4b727d19
MadhuriPriyaPenneru <114864471+MadhuriPriyaPenneru@users.noreply.github.com>
2023-08-08 22:04:19 +0530

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+<script>
+        const registrationForm = document.getElementById("registration-form");
+        const userTable = document.getElementById("user-table");
 
+        registrationForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const dob = document.getElementById("dob").value;
+            const acceptedTerms = document.getElementById("accepted-terms").checked;
+
+            const newRow = userTable.insertRow();
+            newRow.innerHTML = `
+                <td>${name}</td>
+                <td>${email}</td>
+                <td>${password}</td>
+                <td>${dob}</td>
+                <td>${acceptedTerms ? "Yes" : "No"}</td>
+            `;
+
+            // Clear form fields
+            registrationForm.reset();
+        });
+    </script>
